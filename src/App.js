1588a61e03eebd067a80d7b54d641519b9d59496
mmervanakbulut <mmakbulut@gmail.com>
2024-10-09 14:49:41 +0300

    Router provider property moved to index js in app js file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import "./App.css";
+import Navbar from "./components/Navbar.js";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Books from "./components/Books.js";
+import AddAuthor from "./components/Operations/AddAuthor.js";
+import AddPublisher from "./components/Operations/AddPublisher.js";
+import AddBook from "./components/Operations/AddBook.js";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/books",
+      element: <Books />,
+    },
+    {
+      path: "/addAuthor",
+      element: <AddAuthor />,
+    },
+    {
+      path: "/addPublisher",
+      element: <AddPublisher />,
+    },
+    {
+      path: "/addBook",
+      element: <AddBook />,
+    },
+  ]);
   return (
     <div className="App">
-      <header></header>
+      <Navbar />
+      <RouterProvider router={router} />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,38 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import App from "./App.js";
 import reportWebVitals from "./reportWebVitals";
-import Navbar from "./components/Navbar.js";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Books from "./components/Books.js";
-import AddAuthor from "./components/Operations/AddAuthor.js";
-import AddPublisher from "./components/Operations/AddPublisher.js";
-import AddBook from "./components/Operations/AddBook.js";
-
-const router = createBrowserRouter([
-  {
-    path: "/books",
-    element: <Books />,
-  },
-  {
-    path: "/addAuthor",
-    element: <AddAuthor />,
-  },
-  {
-    path: "/addPublisher",
-    element: <AddPublisher />,
-  },
-  {
-    path: "/addBook",
-    element: <AddBook />,
-  },
-]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Navbar />
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
 
